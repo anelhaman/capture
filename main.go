bc3fd294f08d8e0997ebb1f348ebe19d716cc598
Rachen Phengsang <rachen@Rachens-MBP.local>
2023-07-25 08:13:07 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,56 @@
+// Command screenshot is a chromedp example demonstrating how to take a
+// screenshot of a specific element and of the entire browser viewport.
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	// create context
+	ctx, cancel := chromedp.NewContext(
+		context.Background(),
+		// chromedp.WithDebugf(log.Printf),
+	)
+	defer cancel()
+
+	// capture screenshot of an element
+	var buf []byte
+
+	// capture entire browser viewport, returning png with quality=90
+	if err := chromedp.Run(ctx,
+		chromedp.EmulateViewport(1920, 1080),
+		fullScreenshot(`https://evme.io/`, 90, &buf)); err != nil {
+		log.Fatal(err)
+	}
+	if err := os.WriteFile("fullScreenshot.png", buf, 0o644); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("wrote elementScreenshot.png and fullScreenshot.png")
+}
+
+// elementScreenshot takes a screenshot of a specific element.
+func elementScreenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		chromedp.Screenshot(sel, res, chromedp.NodeVisible),
+	}
+}
+
+// fullScreenshot takes a screenshot of the entire browser viewport.
+//
+// Note: chromedp.FullScreenshot overrides the device's emulation settings. Use
+// device.Reset to reset the emulation and viewport settings.
+func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		chromedp.Sleep(3 * time.Second),
+		chromedp.FullScreenshot(res, quality),
+	}
+}
